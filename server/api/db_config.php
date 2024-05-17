9ecae5470ccf3be1a149c56aedbce7d330440f5e
Enki <39771190+Enki013@users.noreply.github.com>
2024-05-17 22:43:01 +0300

    rev

--- server/api/db_config.php
+++ server/api/db_config.php
@@ -1,5 +1,5 @@
 <?php
-// Database configuration
+// Veritabanı yapılandırması
 $db_host = '192.168.1.2';
 $db_port = '3306';
 $db_name = 'my_db';
@@ -7,12 +7,13 @@ $db_user = 'admin';
 $db_pass = 'adminpss';
 
 try {
-    // Establish database connection
+    // Veritabanı bağlantısını kur
     $db_cnn = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name;charset=utf8", $db_user, $db_pass);
     $db_cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    // Handle connection errors
-    echo "Connection error: " . $e->getMessage();
-    // You may want to log this error or handle it differently in a production environment
+    // Bağlantı hatalarını işle
+    header('Content-Type: text/html; charset=UTF-8');
+    echo "Bağlantı hatası: " . $e->getMessage();
+    // Bu hatayı loglamak veya üretim ortamında farklı şekilde ele almak isteyebilirsiniz
 }
 ?>
--- server/api/get_prompt_cards.php
+++ server/api/get_prompt_cards.php
@@ -14,16 +14,16 @@ require_once 'check_session.php';
 try {
     $statement = $db_cnn->prepare("SELECT * FROM prompt_cards");
     $statement->execute();
+
     $prompt_cards = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    header('Content-Type: application/json; charset=UTF-8');
     http_response_code(200);
+    header('Content-Type: application/json; charset=UTF-8');
     echo json_encode($prompt_cards);
 } catch (PDOException $e) {
-    // Handle database errors gracefully
-    http_response_code(500); // Internal Server Error
-    echo json_encode(array("error" => "Database error: " . $e->getMessage()));
+    // Veritabanı hatalarını düzgün bir şekilde ele al
+    http_response_code(500); // İç Sunucu Hatası
+    echo json_encode(array("error" => "Veritabanı hatası: " . $e->getMessage()));
 }
-
 
 ?>
